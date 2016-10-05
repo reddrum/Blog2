8a264c45d2b5c1833558451220f1405e7302f618
“reddrum” <anton.kranz@me.com>
2016-10-05 23:32:01 +0300

    Add paperclip

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: {minimum: 5}
   validates :body,  presence: true
+
+  has_attached_file :image, styles: { medium: "500x500>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
